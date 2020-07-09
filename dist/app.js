7ea84a9bbae610cf8edd961068221e2f0e047c81
Laura <lstorella10@gmail.com>
2020-07-10 00:22:57 +0200

    Base Layout | Header-Main

--- dist/app.js
+++ dist/app.js
@@ -86,21 +86,21 @@
 /************************************************************************/
 /******/ ({
 
-/***/ "./src/app.js":
-/*!********************!*\
-  !*** ./src/app.js ***!
-  \********************/
+/***/ "./src/js/app.js":
+/*!***********************!*\
+  !*** ./src/js/app.js ***!
+  \***********************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-
+$(document).ready(function () {}); // document ready
 
 /***/ }),
 
-/***/ "./src/app.scss":
-/*!**********************!*\
-  !*** ./src/app.scss ***!
-  \**********************/
+/***/ "./src/scss/app.scss":
+/*!***************************!*\
+  !*** ./src/scss/app.scss ***!
+  \***************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -109,14 +109,14 @@
 /***/ }),
 
 /***/ 0:
-/*!*****************************************!*\
-  !*** multi ./src/app.js ./src/app.scss ***!
-  \*****************************************/
+/*!*************************************************!*\
+  !*** multi ./src/js/app.js ./src/scss/app.scss ***!
+  \*************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\lstor\Desktop\BooleanRepo\node-sass-teambit\src\app.js */"./src/app.js");
-module.exports = __webpack_require__(/*! C:\Users\lstor\Desktop\BooleanRepo\node-sass-teambit\src\app.scss */"./src/app.scss");
+__webpack_require__(/*! C:\Users\lstor\Desktop\BooleanRepo\node-sass-teambit\src\js\app.js */"./src/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\lstor\Desktop\BooleanRepo\node-sass-teambit\src\scss\app.scss */"./src/scss/app.scss");
 
 
 /***/ })
